e25323d8a494c1f999e85d91f54c3be6b8a1ed45
Stan125 <stani@StaniBook.home>
2016-03-10 18:07:18 +0100

    Cute Cat

--- R/limoaddin.R
+++ R/limoaddin.R
@@ -5,16 +5,14 @@ library(miniUI)
 
 # We'll wrap our Shiny Gadget in an addin.
 # Let's call it 'clockAddin()'.
-clockAddin <- function() {
+sushicat <- function() {
   
   # Our ui will be a simple gadget page, which
   # simply displays the time in a 'UI' output.
   ui <- miniPage(
-    gadgetTitleBar("Clock"),
-    h2("When is Guillermo a bitch?", align = "middle"),
-    miniContentPanel(
-      uiOutput("time")
-    )
+    gadgetTitleBar("Cute Cat"),
+    miniContentPanel(img(src = "http://costumewall.com/wp-content/uploads/2015/09/cute-cat-costumes-30.jpg",
+                         style = "display: block; margin: 0 auto;"))
   )
   
   server <- function(input, output, session) {
@@ -30,42 +28,19 @@ clockAddin <- function() {
       sep = "; "
     )
     
-    # We'll use a 'reactiveTimer()' to force Shiny
-    # to update and show the clock every second.
-    invalidatePeriodically <- reactiveTimer(intervalMs = 1000)
-    observe({
-      
-      # Call our reactive timer in an 'observe' function
-      # to ensure it's repeatedly fired.
-      invalidatePeriodically()
-      
-      # Get the time, and render it as a large paragraph element.
-      time <- Sys.time()
-      output$time <- renderUI({
-        p(style = clockStyles, time)
-      })
-    })
-    
-    # Listen for 'done' events. When we're finished, we'll
-    # insert the current time, and then stop the gadget.
     observeEvent(input$done, {
-      timeText <- paste0("\"", as.character(Sys.time()), "\"")
-      rstudioapi::insertText(timeText)
       stopApp()
     })
     
-  }
+    }
   
   # We'll use a pane viwer, and set the minimum height at
   # 300px to ensure we get enough screen space to display the clock.
-  viewer <- dialogViewer(dialogName = "Clock Add-In", 
+  viewer <- dialogViewer(dialogName = "Sushi Cat", 
                          height = 600, 
-                         width = 1200)
+                         width = 900)
   runGadget(ui, server, viewer = viewer)
-  
 }
-
-clockAddin()
 
 # Now all that's left is sharing this addin -- put this function
 # in an R package, provide the registration metadata at
